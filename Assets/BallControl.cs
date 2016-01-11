caf7017d23a7091a077d84822335c2f12c107f4f
Tammy Tyberg <tammy.tyberg@smallplanet.com>
2016-01-11 17:45:52 -0500

    firstCommit

---
+++ Assets/BallControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+	
+	public int rotationSpeed = 100;
+	public Rigidbody rb;
+	
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called Bonce per frame
+	void Update () {
+		
+		
+		//respond to  A and D keys to rotate
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		rotation *= Time.deltaTime;
+		
+		//want to rotate rigid body using vector3.back since that'ts axis we r working with
+		//multiply based on rotation speed and based on deltaTime so not on frame rate
+		rb.AddRelativeTorque (Vector3.back * rotation);
+		
+	}
+}
