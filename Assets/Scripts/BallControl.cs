75ebb708923399fd5593b03bcab56d3bdc8a0988
Tammy Tyberg <tammy.tyberg@smallplanet.com>
2016-01-12 12:16:15 -0500

    finished 5 tutorials added more blocks and assets

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+	
+	public int rotationSpeed = 100;
+	public Rigidbody rb, rb1;
+	public int jumpHeight = 8;
+	private bool isFalling = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called Bonce per frame
+	void Update () {
+		
+		//handle ball rotation 
+		//respond to  A and D keys to rotate
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		rotation *= Time.deltaTime;
+		
+		//want to rotate rigid body using vector3.back since that'ts axis we r working with
+		//multiply based on rotation speed and based on deltaTime so not on frame rate
+		rb.AddRelativeTorque (Vector3.back * rotation);
+
+
+
+		//if user presses space for jump
+		if (Input.GetKeyDown (KeyCode.Space)  && !isFalling) {
+
+
+			//set velocity in y axis to go up to 8 in y axis
+			Vector3 v = rb.velocity;
+			v.y = jumpHeight;
+			rb.velocity = v;
+
+
+
+		}
+
+		//not touching an object so in air
+		isFalling = true;
+
+	}
+
+	//called once per frame for object
+	void OnCollisionStay(Collision collisionInfo){
+
+		isFalling = false;
+	}
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+
+   public Transform target;
+	public int distance = -10; //so camera baxked up ten units in set axis
+	public float lift = 1.5f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//position of this object which is the camera so I guess it's the object you're scripting on
+
+		transform.position = target.position + (new Vector3 (0, lift, distance));//set positon based on vector, want to set to balls x
+		//so stays zero, want equal to y plus 1.5 so put lift, and add distance to -10 this works since we set target
+		//to the ball object in inspector so camera pos should be equal to ball distance plus modifications
+
+
+		//when ball goes up want to camera to look at ball so say camera look at the target (which is the ball)
+		transform.LookAt (target);
+
+	
+	}
+}
