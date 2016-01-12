eadaad274b2e1e2d5435bda4059926e17a8742c8
Tammy Tyberg <tammy.tyberg@smallplanet.com>
2016-01-12 13:20:48 -0500

    added ballhealth.cs to catch when ball falls and reload the level but is missing light when reload level

---
+++ Assets/Scripts/BallHealth.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallHealth : MonoBehaviour {
+	public int maxFallDistance = -10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//script is sittion on ball so use transform to get ball pos
+		if (transform.position.y <= maxFallDistance) {
+
+			//normally let ball jump back to checkpoint but only have one level now
+			//so just reload the level
+			Application.LoadLevel("Level1");
+			//Debug.Log ("Testing ball falling script");
+		}
+
+	}
+}
