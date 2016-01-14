8cf4c77fa610dc097578d3e62a69a959b9d2ef63
Tammy Tyberg <tammy.tyberg@smallplanet.com>
2016-01-14 10:59:10 -0500

    added logic to count logs once upon first collision

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameMaster : MonoBehaviour {
 
 	public static int currentScore = 0;
+	public static int logCounter = 0;
 	public int scoreInspector = currentScore;
 	public int scoreY = Screen.height - (Screen.height - 30);
 	public int scoreSizeX = 200;
@@ -15,16 +16,15 @@ public class GameMaster : MonoBehaviour {
 
 	void Update(){
 
-		scoreInspector = currentScore;
+		//scoreInspector = currentScore;
 
 	}
 	
 	// Update is called once per frame
 	void OnGUI () {
 	
-		GUI.Box (new Rect (Screen.width/3, scoreY, scoreSizeX, scoreSizeY ), "Score: " + currentScore);
-
-
+		GUI.Box (new Rect (Screen.width/3, scoreY, scoreSizeX, scoreSizeY ),
+		         "Score: " + currentScore + "Logs:" + logCounter);
 
 	}
 }
